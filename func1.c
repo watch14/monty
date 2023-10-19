7e33a36090f5643d27b4b752e6bcea53006c4f72
watch14 <watchman1414@gmail.com>
2023-10-19 09:40:36 +0000

    fast_push

--- func1.c
+++ func1.c
@@ -53,7 +53,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!stack || !*stack)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
